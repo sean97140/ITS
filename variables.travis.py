ee845b94e08bfb7e97e25ef7364be3f7b51abac5
Matt Johnson <mdj2@pdx.edu>
2015-01-15 16:32:58 -0800

    Travis stuff

---
+++ variables.travis.py
@@ -0,0 +1,37 @@
+DEBUG = True
+
+ALLOWED_HOSTS = '*'
+
+# List of two-tuples containing your name, and an email [("Joe", "joe@example.com")]
+ADMINS = []
+
+# if you're having trouble connecting to LDAP set this to True so you can login
+# to track, bypassing LDAP group checks
+LDAP_DISABLED = False
+
+# ('Your Name', 'your_email@example.com'),
+ADMINS = []
+
+DB_NAME = 'its'
+
+DB_USER = 'root'
+
+DB_PASSWORD = ''
+
+DB_HOST = ''
+
+SECRET_KEY = 'foobar'
+
+# In dev: django.core.mail.backends.console.EmailBackend
+# In prod: django.core.mail.backends.smtp.EmailBackend
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+LDAP_HOST = 'ldap://ldap-bulk.oit.pdx.edu'
+
+LDAP_USERNAME = 'uid=rethinkwebsite,ou=service,dc=pdx,dc=edu'
+
+LDAP_PASSWORD = ''
+
+LDAP_SEARCH_DB = 'ou=people,dc=pdx,dc=edu'
+
+LDAP_TLS = True
