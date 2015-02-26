b35bc8cf34288a2e2b785ffe929f26f8f497d2bc
Paul Thompson <pbt@pdx.edu>
2015-02-26 11:47:30 -0800

    Removed password field from user admin form

--- its/users/admin.py
+++ its/users/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import User
 
-admin.site.register(User)
+
+# Since we are using CAS we don't want users to
+# worry about setting any kind of password.
+class UserAdmin(admin.ModelAdmin):
+    exclude = ('password',)
+
+admin.site.register(User, UserAdmin)
