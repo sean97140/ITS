ec5f398b2b2268eb62039908864807eb0aadfde3
Paul Thompson <pbt@pdx.edu>
2015-03-23 15:24:28 -0700

    Added try except

--- its/users/management/commands/createadmin.py
+++ its/users/management/commands/createadmin.py
@@ -11,7 +11,12 @@ class Command(BaseCommand):
         last = input("Enter your last name: ")
         new_email = input("Enter your email address: ")
         odin = input("Enter your ODIN username: ")
-        
-        new_user = User(first_name = first, last_name = last, email = new_email, username = odin, is_active=True, is_staff=True)
-        new_user.save()
+
+        try:
+            new_user = User(first_name = first, last_name = last, email = new_email, username = odin, is_active=True, is_staff=True)
+            new_user.save()
+            
+        except Exception as e:
+            self.stdout.write('Unable to add user!')
+
         self.stdout.write('Successfully added admin user!')
