ea37943c52b7ad52e2440042e1b5bb2f7ce2f60c
Paul Thompson <pbt@pdx.edu>
2015-03-27 18:06:59 -0700

    Added django-perms

--- its/settings.py
+++ its/settings.py
@@ -97,6 +97,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'permissions',
     'arcutils',
     'its.users',
 	'its.items',
